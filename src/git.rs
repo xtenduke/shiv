6ef6f5f90b6c82e09c7d8b2bb6ab82cc896f7baa
Jake Laurie <5002212+xtenduke@users.noreply.github.com>
2023-07-01 11:32:30 +0800

    Change diff to use workdir and remote branch to work in detached head state (#11)

--- src/git.rs
+++ src/git.rs
@@ -1,8 +1,39 @@
 use git2::{Repository, BranchType, Branch, Tree, DiffDelta};
 
-pub struct ComparisonBranches<'a> {
-    head: Branch<'a>,
-    main: Branch<'a>,
+/// Get main branch reference, either from local checked out branch, or pull from remote config
+pub fn get_main_branch_ref<'a>(repo: &'a Repository, main_branch: &String) -> Branch<'a> {
+    let main: Option<Branch> = match repo.find_branch(&main_branch, BranchType::Remote) {
+        Ok(main) => Some(main),
+        Err(_) => None,
+    };
+
+    return if main.is_none() {
+        // Fallback to remote
+        // find current remote
+
+        // pick first remote.. lol
+        let remote_names = match repo.remotes() {
+            Ok(remote_names) => remote_names,
+            Err(e) => panic!("Failed to get remote names {}", e),
+        };
+
+        // stringbuilder i.e. `origin/main`
+        let mut remote_name = match remote_names.get(0) {
+            Some(remote_name) => remote_name.to_string(),
+            None => panic!("Can't find main branch locally, can't see any remotes.")
+        };
+
+        remote_name.push_str("/");
+        remote_name.push_str(&main_branch);
+        let remote_main = match repo.find_branch(&remote_name, BranchType::Remote) {
+            Ok(main) => main,
+            Err(e) => panic!("Unable to find main branch named {} because {}", main_branch, e),
+        };
+
+        remote_main
+    } else {
+        main.unwrap()
+    }
 }
 
 pub fn get_changed_files(root_dir: &String, main_branch: &String) -> Vec<String> {
@@ -11,26 +42,17 @@ pub fn get_changed_files(root_dir: &String, main_branch: &String) -> Vec<String>
         Err(e) => panic!("Failed to open: {}", e),
     };
 
-    let branches = match get_branches(&repo, &main_branch) {
-        Ok(references) => references,
-        Err(e) => panic!("Failed to get a reference: {}", e),
-    };
-
-    let head_tree: Tree = match branches.head.into_reference().peel_to_tree() {
-        Ok(head_tree) => head_tree,
-        Err(e) => panic!("Failed getting tree from head branch ref: {}", e),
-    };
-
-    let main_tree: Tree = match branches.main.into_reference().peel_to_tree() {
+    let remote_main = get_main_branch_ref(&repo, &main_branch);
+    let main_tree: Tree = match remote_main.into_reference().peel_to_tree() {
         Ok(head_tree) => head_tree,
         Err(e) => panic!("Failed getting tree from main branch ref: {}", e),
     };
 
-    let diff = match repo.diff_tree_to_tree(Some(&main_tree), Some(&head_tree), None) {
+    let diff = match repo.diff_tree_to_workdir_with_index(Some(&main_tree), None) {
         Ok(diff) => diff,
         Err(e) => panic!("Diff failed: {}", e),
     };
-    
+
     let mut changes: Vec<String> = Vec::new();
 
     let mut fun = |diff: DiffDelta<'_>, _: f32| -> bool {
@@ -42,8 +64,8 @@ pub fn get_changed_files(root_dir: &String, main_branch: &String) -> Vec<String>
         if new_file != old_file {
             changes.push(old_file.to_owned());
         }
-        
-        return true; 
+
+        return true;
     };
 
     let _ = match diff.foreach(&mut fun, None, None, None) {
@@ -52,53 +74,4 @@ pub fn get_changed_files(root_dir: &String, main_branch: &String) -> Vec<String>
     };
     
     return changes;
-}
-
-fn get_branches<'a>(repo: &'a Repository, main_branch: &str) -> Result<ComparisonBranches<'a>, & 'a str> {
-    let mut head: Option<Branch> = None;
-    let mut main: Option<Branch> = None;
-
-    // list local branches, need full clone
-    let branches = match repo.branches(Some(BranchType::Local)) {
-        Ok(branches) => branches,
-        Err(e) => panic!("Failed to get branches: {}", e),
-    };
-    for branch in branches {
-        let branch = match branch {
-            Ok(branch) => branch.0,
-            Err(_) => continue,
-        };
-
-        let beans = &branch.name();
-        let name = match beans {
-            Ok(name) => name,
-            Err(_) => continue,
-        };
-
-        let name = match name {
-            Some(name) => name,
-            None => continue,
-        };
-
-        if branch.is_head() {
-            println!("found head branch: {}", &name);
-            head = Some(branch);
-        } else if name == &main_branch {
-            println!("found main branch: {}", &name);
-            main = Some(branch);
-        }
-    }
-
-    if head.is_none() {
-        return Err("Unable to find head reference");
-    }
-
-    if main.is_none() {
-        return Err("Unable to find main reference");
-    }
-
-    return Ok(ComparisonBranches {
-        head: head.unwrap(),
-        main: main.unwrap()
-    });
 }
