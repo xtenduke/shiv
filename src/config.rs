48584221d4c73d7e3deff66c09c33b392466f823
Jake Laurie <5002212+xtenduke@users.noreply.github.com>
2023-12-14 09:18:17 +0800

    Remove bad panic from config read (#13)

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::fs;
+use std::{fs, error::Error};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
@@ -12,16 +12,22 @@ pub struct Scripts {
     pub run: String
 }
 
-pub fn load_config(absolute_path: &String) -> Config {
+pub fn load_config(absolute_path: &String) -> Result<Config, Box<dyn Error>> {
     let res = match fs::read_to_string(&absolute_path) {
         Ok(res) => res,
-        Err(e) => panic!("Failed to read config file: {}: {}", &absolute_path, e),
+        Err(e) => {
+            eprintln!("Failed reading config file, does it exist? {}", &absolute_path);
+            return Err(e.into());
+        }
     };
 
     let config: Config = match serde_json::from_str(&res) {
         Ok(config) => config,
-        Err(e) => panic!("Failed to load config: {}", e)
+        Err(e) => {
+            eprintln!("Failed parsing config file {}, is it valid json?", &absolute_path);
+            return Err(e.into());
+        }
     };
 
-    return config;
+    return Ok(config);
 }
